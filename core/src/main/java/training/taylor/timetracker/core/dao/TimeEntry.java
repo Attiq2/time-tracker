7967de30b2feb6b3fe8b0c5e55a41e261e181d95
Attiq2 <161135367+Attiq2@users.noreply.github.com>
2025-02-11 17:55:51 +0500

    Update TimeEntry.java

--- core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
+++ core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
@@ -1,40 +1,52 @@
 package training.taylor.timetracker.core.dao;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
+import org. spring framework.context.annotation.Scope;
+import org. spring framework. stereotype.Component;
+import java.math.BigDecimal;
 
 /**
  * Created by Jason on 6/19/2015.
  */
 @Component
-@Scope("prototype")
+@Scope("prototype") // Remove if singleton behavior is preferred
 public class TimeEntry {
     private String description;
-    private float rate;
+    private BigDecimal rate; // Use BigDecimal for monetary values
     private int time;
+
+    // Default constructor
+    public TimeEntry() {
+    }
+
+    // Parameterized constructor
+    public TimeEntry(String description, BigDecimal rate, int time) {
+        this.description = description;
+        this.rate = rate;
+        this.time = time;
+    }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String value) {
-        this.description = value;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public float getRate() {
+    public BigDecimal getRate() {
         return rate;
     }
 
-    public void setRate(float value) {
-        this.rate = value;
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
     }
 
     public int getTime() {
         return time;
     }
 
-    public void setTime(int value) {
-        this.time = value;
+    public void setTime(int time) {
+        this.time = time;
     }
 
     @Override
