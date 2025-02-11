c32c8b1f501fc6bc3497b4c13c5cfbf830c5f861
Attiq2 <161135367+Attiq2@users.noreply.github.com>
2025-02-11 18:05:27 +0500

    Update TrackerCoreConfigTest.java

--- core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
@@ -1,29 +1,23 @@
-package training.taylor.timetracker.core;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import training.taylor.timetracker.core.dao.TimeEntry;
-
+import org. spring framework.context.annotation.Bean;
+import org. spring framework.context.annotation.Configuration;
+import training. Taylor. time-tracker.core.dao.TimeEntry;
+import java.util.Arrays;
 import java.util.List;
 
-/**
- * Created by Jason on 6/20/2015.
- */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TrackerCoreConfig.class)
-public class TrackerCoreConfigTest {
-
-    @Autowired
-    List<TimeEntry> entries;
-
-    @Test
-    public void testMe() {
-        assertNotNull(entries);
+@Configuration
+public class TrackerCoreConfig {
+    @Bean
+    public TimeEntry timeEntry1() {
+        return new TimeEntry("Task A", new BigDecimal("50.0"), 5);
     }
 
+    @Bean
+    public TimeEntry timeEntry2() {
+        return new TimeEntry("Task B", new BigDecimal("75.0"), 3);
+    }
+
+    @Bean
+    public List<TimeEntry> timeEntries() {
+        return Arrays.asList(timeEntry1(), timeEntry2());
+    }
 }
