849a042c9bc1eb83fe31224a584bcb8d905a5052
Attiq2 <161135367+Attiq2@users.noreply.github.com>
2025-02-11 18:10:07 +0500

    Update TrackerTest.java

--- core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
@@ -1,35 +1,39 @@
-package training.taylor.timetracker.core;
+package training.Taylor.time-tracker.core;
 
-import static org.junit.Assert.*;
+import static org. junit.Assert.*;
 
-import org.junit.Test;
+import org. junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import training.taylor.timetracker.core.dao.TimeEntry;
+import org. spring framework.beans.factory.annotation.Autowired;
+import org. spring framework.test.context.ContextConfiguration;
+import org. spring framework.test.context.junit4.SpringJUnit4ClassRunner;
+import training. Taylor. time-tracker.core.dao.TimeEntry;
+
+import java.math.BigDecimal;
 
 /**
  * Created by Jason on 6/19/2015.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TrackerCoreConfig.class)
+@ContextConfiguration(classes = TrackerCoreConfig.class) // Ensure this config exists
 public class TrackerTest {
+
     @Autowired
-    private Tracker tracker;
+    private Tracker tracker; // Ensure Tracker is a valid Spring bean
 
     @Test
     public void testMe() {
-        assertNotNull(tracker);
+        assertNotNull("Tracker bean should not be null", tracker);
     }
 
     @Test
     public void testAdd() {
         TimeEntry entry = new TimeEntry();
         entry.setDescription("Entry Test");
-        entry.setRate(80.0f);
+        entry.setRate(new BigDecimal("80.0")); // Use BigDecimal instead of float
         entry.setTime(3);
-        tracker.add(entry);
-        assertTrue(tracker.size() > 0);
+
+        tracker.add(entry); // Ensure `Tracker` has an `add(TimeEntry entry)` method
+        assertTrue("Tracker should contain at least one entry", tracker.size() > 0);
     }
 }
