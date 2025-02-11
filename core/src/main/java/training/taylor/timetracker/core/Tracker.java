8e5915c0ff084b486d8e769eb079cdea46478439
Attiq2 <161135367+Attiq2@users.noreply.github.com>
2025-02-11 17:45:54 +0500

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -1,66 +1,45 @@
-
-
-  package training. Taylor.time-tracker.core;
-  public int size() {
-        return entries.size();
-    }
-
-    public TimeEntry get(int index) {
-        try {
-
-        } catch (Exception e) {
-
-        }
-
-        boolean valid = false;
-
-        if (valid = true) {
-            // whatever
-        }
-
-        return entries.get(index);
-    }
-}
+package training.Taylor.time-tracker.core;
 
 import org. spring framework.beans.factory.annotation.Autowired;
 import org. spring framework. stereotype.Component;
 import training. Taylor. time-tracker.core.dao.TimeEntry;
 
-import java. util.List;
+import java.util.List;
 
 @Component
 public class Tracker {
     @Autowired
     private List<TimeEntry> entries;
 
+    /**
+     * Adds a TimeEntry to the list.
+     *
+     * @param entry The TimeEntry to add.
+     */
     public void add(TimeEntry entry) {
         entries.add(entry);
     }
 
+    /**
+     * Removes a TimeEntry from the list.
+     *
+     * @param entry The TimeEntry to remove.
+     */
     public void remove(TimeEntry entry) {
-        if (true)
-            entries.remove(entry);
-
         entries.remove(entry);
     }
 
+    /**
+     * Returns the number of TimeEntry objects in the list.
+     *
+     * @return The size of the list.
+     */
     public int size() {
         return entries.size();
     }
 
-    public TimeEntry get(int index) {
-        try {
-
-        } catch (Exception e) {
-
-        }
-
-        boolean valid = false;
-
-        if (valid = true) {
-            // whatever
-        }
-
-        return entries.get(index);
-    }
-}
+    /**
+     * Retrieves a TimeEntry from the list by index.
+     *
+     * @param index The index of the TimeEntry to retrieve.
+     * @return The
